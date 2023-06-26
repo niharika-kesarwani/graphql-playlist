7ddbc86cba0657d5a40f1d122ac52e7eeb3ac0b1
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-06-26 17:23:15 +0530

    book list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+// components
+import BookList from "./components/BookList";
+
 function App() {
   return (
     <div id="main">
       <h1>Ninja's Reading List</h1>
+      <BookList />
     </div>
   );
 }
