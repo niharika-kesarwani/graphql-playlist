2970fb894d2ef44b8cc6eaa718e4045fa2027a96
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-06-27 19:14:35 +0530

    apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
+import { ApolloClient, ApolloProvider } from "@apollo/client";
+
 // components
 import BookList from "./components/BookList";
 
+// apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
+
 function App() {
   return (
-    <div id="main">
-      <h1>Ninja's Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ninja's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
