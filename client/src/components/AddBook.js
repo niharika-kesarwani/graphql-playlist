7fbf328b629ffec3b04183efc76b697a2783f261
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-06-29 21:04:35 +0530

    updating component state

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,9 +1,15 @@
 import { useQuery } from "@apollo/client";
 import { graphql } from "@apollo/client/react/hoc";
 import { getAuthorsQuery } from "../queries/queries";
+import { useState } from "react";
 
 function AddBook() {
   const { data, loading, error } = useQuery(getAuthorsQuery);
+  const [formDetails, setFormDetails] = useState({
+    name: "",
+    genre: "",
+    authorId: "",
+  });
 
   if (loading) {
     return <option disabled>Loading Authors...</option>;
@@ -13,21 +19,40 @@ function AddBook() {
     return <option disabled>Something went wrong!</option>;
   }
 
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(formDetails);
+  };
+
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={formSubmitHandler}>
       <div className="field">
         <label>Book name:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) =>
+            setFormDetails({ ...formDetails, name: e.target.value })
+          }
+        />
       </div>
 
       <div className="field">
         <label>Genre:</label>
-        <input type="label" />
+        <input
+          type="label"
+          onChange={(e) =>
+            setFormDetails({ ...formDetails, genre: e.target.value })
+          }
+        />
       </div>
 
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select
+          onChange={(e) =>
+            setFormDetails({ ...formDetails, authorId: e.target.value })
+          }
+        >
           <option>Select author</option>
           {data.authors.map((author) => (
             <option key={author.id} value={author.id}>
